83aaf97cf74b0411f55370442cc7e66957889dc8
jussantxtecnologia <jussan.silva@txtecnologia.com.br>
2024-06-19 14:53:45 -0300

    Ajustes para Login

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,24 +1,37 @@
-import React from "react";
-import { Text, View } from "react-native";
-import styles from "../config/styles";
-import { Image } from "react-native";
-import { auth } from "../config/firebase";
-
-
+import { useState, useEffect } from 'react'; // Apenas importe useState e useEffect, não é necessário importar React novamente
+import { Text, View, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from '../config/styles';
 
 export default function HomeScreen() {
+  const [userName, setUserName] = useState(''); // cria o estado do nome do usuário
+  
+  useEffect(() => {
+    const getUserName = async () => {
+      try {
+        const storedName = await AsyncStorage.getItem('userName');
+        if (storedName) {
+          setUserName(storedName);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar nome do usuário", error);
+      }
+    };
+
+    getUserName();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image
-          source={require("../../assets/LogoApp.png")}
-          style={{ width: 260, height: 120, alignSelf: "center" }}
-        />
-      <Text style={[styles.h1, { textAlign: 'center', margin: 10, fontSize: 36 }]}>Olá, </Text>
+        source={require("../../assets/LogoApp.png")}
+        style={{ width: 260, height: 120, alignSelf: "center" }}
+      />
+      <Text style={[styles.h1, { textAlign: 'center', margin: 10, fontSize: 36 }]}>Olá, {userName}</Text>
       <Text style={[styles.h1, { textAlign: 'center', margin: 10, fontSize: 24 }]}>Bem vindo ao App "Cond Segurity"</Text>
       <Text style={{ textAlign: 'justify', margin: 10 }}>
-      O propósito essencial deste projeto é conceber um sistema de controle de acesso automatizado para veículos direcionado a condomínios residenciais na cidade de Joinville - SC, o objetivo principal é garantir uma entrada eficaz e segura para veículos autorizados.
+        O propósito essencial deste projeto é conceber um sistema de controle de acesso automatizado para veículos direcionado a condomínios residenciais na cidade de Joinville - SC, o objetivo principal é garantir uma entrada eficaz e segura para veículos autorizados.
       </Text>
-      
     </View>
   );
 }
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,9 +3,8 @@ import { Button, Text, TextInput } from "react-native-paper";
 import { useEffect, useState } from "react"; 
 import styles from "../config/styles";
 import { Image } from "expo-image"; 
-import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth"; // importa o método de autenticação do Firebase
-import { auth } from "../config/firebase"; // importa a instância do Firebase
 import AsyncStorage from '@react-native-async-storage/async-storage'; // importa o AsyncStorage para armazenar o token de autenticação
+import React from "react";
 
 export default function LoginScreen({ navigation }) { // passa a navegação como parâmetro
   const [email, setEmail] = useState(""); // cria o estado do email
@@ -40,7 +39,28 @@ export default function LoginScreen({ navigation }) { // passa a navegação com
       console.log(data); // exibe os dados no console
       await AsyncStorage.setItem('token', data.token); // armazena o token de autenticação no AsyncStorage
       console.log('Usuário logado com sucesso!'); // exibe a mensagem de sucesso no console
-      navigation.navigate('HomeScreen'); // navega para a tela HomeScreen
+      const UserId = data.userId; // pega o ID do usuário logado que retornou da API
+      const userResponse = await fetch(`https://apicondsecurity.azurewebsites.net/api/Usuario/Get?id=${UserId}`,
+      { method: 'GET', 
+        headers: { 
+          'Authorization': `Bearer ${data.token}`, // passa o token de autenticação no cabeçalho da requisição
+          'Content-type': 'application/json', // define o tipo do conteúdo como JSON
+          mode: 'cors', // Adiciona o modo CORS
+        },
+    }); // faz a requisição para a API com o ID do usuário
+    if(!userResponse.ok){ // verifica se a resposta não está ok
+      throw new Error('Erro ao buscar usuário'); // lança um erro
+    }
+    const userData = await userResponse.json(); // converte a resposta para JSON
+    const userType = userData.idTipoUsuario; // converte a resposta para JSON
+    const userName = userData.nome; // pega o nome do usuário logado
+    await AsyncStorage.setItem('userType', userType); // armazena o tipo de usuário no AsyncStorage
+    await AsyncStorage.setItem('userName', userName); // armazena o tipo de usuário no AsyncStorage
+    if (userType === 1) { // verifica se o tipo de usuário é 1 Administrador
+      navigation.navigate('HomeScreen'); // navega para a tela HomeScreenAdmin
+    } else {
+      navigation.navigate('HomeScreen'); // trocar para a tela HomeScreenUser quando estiver pronta
+    }
     } catch (error) { // captura o erro
       console.log('Erro ao fazer login',error); // exibe o erro no console
     }
@@ -62,9 +82,13 @@ export default function LoginScreen({ navigation }) { // passa a navegação com
     const checkUserLoggedIn = async () => {
       try {
         const token = await AsyncStorage.getItem('token');
-        if (token) {
+        const userType = await AsyncStorage.getItem('userType');
+        if (token && userType === 1) {
           navigation.navigate("HomeScreen");
-        } else {
+        } else if (token && userType != 2) {
+                  navigation.navigate("HomeScreen"); // trocar para a tela HomeScreenUser quando estiver pronta
+                }
+          else {
           setLoading(false);
         }
       } catch (error) {
